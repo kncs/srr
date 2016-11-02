f907e73ba0181f0e9906728589d3553c57983647
Kevin Clarens <kevin.clarens@gmail.com>
2016-11-02 21:56:09 +0100

    tests(actions): install jest and babel-jest, then add  tests for actions

---
+++ tests/actions.test.js
@@ -0,0 +1,18 @@
+import * as actions from '../frontend/actions/counter.actions'
+import * as types from '../frontend/constants/actionTypes'
+
+describe('actions', () => {
+  it('should create an action to increment counter', () => {
+    const expectedAction = {
+      type: types.INCREMENT
+    }
+    expect(actions.increment()).toEqual(expectedAction)
+  })
+
+  it('should create an action to decrement counter', () => {
+    const expectedAction = {
+      type: types.DECREMENT
+    }
+    expect(actions.decrement()).toEqual(expectedAction)
+  })
+})
